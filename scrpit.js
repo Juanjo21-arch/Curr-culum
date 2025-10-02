c4d48397497b07db2701a7fbc8ba099564f64aff
Juanjo21-arch <jpedraza19781@iesjoanramis.org>
2025-10-02 10:44:23 +0200

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,21 @@
+function canviNom(){
+    let nom = prompt("¿COMO QUIERES LLAMARTE?")
+    document.getElementById("nom").innerHTML = "NOM: "+nom;
+}
+
+function canviFoto(){
+    let foto = prompt("¿QUE QUIERES SER?")
+    document.getElementById("foto").src = foto
+
+}
+
+function harryPotter(){
+
+if(document.getElementById("foto").hidden == true){
+   document.getElementById("foto").hidden == false
+}else{
+    document.getElementById("foto").hidden == true
+}
+
+
+}
